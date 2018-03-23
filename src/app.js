941998276b121d388a5e5f366a48a0fa890c5cbe
mirgj <mirash.gjolaj@gmail.com>
2018-03-23 18:59:40 +0800

    static files configuration

--- src/app.js
+++ src/app.js
@@ -27,6 +27,7 @@ const app = express();
     app.locals.moment = moment;
     app.set('view engine', 'pug');
     app.set('views', path.join(__dirname, '/views'));
+    app.use('/static', express.static(path.join(__dirname, '../public'), { maxAge: config.session.staticMaxAge }));
     app.use(morgan('combined', { stream: expressLogger.stream }));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -49,11 +50,9 @@ const app = express();
     app.use('/api', apiRoutes);
     app.use('/', viewRoutes);
 
-    if (!module.parent) {
-      app.server.listen(config.application.port, config.application.hostname, () => {
-        logger.log('verbose', `Application started on port ${app.server.address().address}:${app.server.address().port}`);
-      });
-    }
+    app.server.listen(config.application.port, config.application.hostname, () => {
+      logger.log('verbose', `Application started on port ${app.server.address().address}:${app.server.address().port}`);
+    });
   } catch (err) {
     logger.error(`Unexpected exception: ${JSON.stringify(err)}`);
   }
