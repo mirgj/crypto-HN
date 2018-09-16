50c2c58d23ac2f80195454c8915f8b42a4b90771
mirgj <mirash.gjolaj@gmail.com>
2018-09-16 22:07:43 +0800

    Add in https support

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import http from 'http';
+import https from 'https';
+import fs from 'fs';
 import express from 'express';
 import path from 'path';
 import morgan from 'morgan';
@@ -23,12 +25,29 @@ const app = express();
   try {
     await connect(config.database.connectionString);
     await initDefaultDb(config.database.defaultDbName);
+    let privateKey;
+    let certificate;
+    let ca;
+    let httpsServer;
 
-    app.server = http.createServer(app);
+    if (config.application.env === "prod") {
+      privateKey = fs.readFileSync(config.application.privateKeyPath, 'utf8');
+      certificate = fs.readFileSync(config.application.certificatePath, 'utf8');
+      ca = fs.readFileSync(config.application.caPath, 'utf8');
+      httpsServer = https.createServer({
+        key: privateKey,
+        cert: certificate,
+        ca: ca
+      }, app);
+    }
+
+    const httpServer = http.createServer(app);
+
     app.locals.moment = moment;
     app.set('view engine', 'pug');
     app.set('views', path.join(__dirname, '/views'));
-    app.use('/static', express.static(path.join(__dirname, 'public'), { maxAge: config.session.staticMaxAge }));
+    app.use('/static', express.static(path.join(__dirname, 'public'), { maxAge: config.session.staticMaxAge, dotfiles: 'allow' }));
+    app.use('/.well-known', express.static(path.join(__dirname, 'public/.well-known'), { dotfiles: 'allow' }));
     app.use(morgan('combined', { stream: expressLogger.stream }));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
@@ -52,8 +71,13 @@ const app = express();
     app.use('/api', apiRoutes);
     app.use('/', viewRoutes);
 
-    app.server.listen(config.application.port, config.application.hostname, () => {
-      logger.log('verbose', `Application started on port ${app.server.address().address}:${app.server.address().port}`);
+    if(httpsServer) {
+      httpsServer.listen(config.application.sslPort, config.application.hostname, () => {
+        logger.log('verbose', `Application (https) started on port ${httpsServer.address().address}:${httpsServer.address().port}`);
+      });
+    }
+    httpServer.listen(config.application.port, config.application.hostname, () => {
+      logger.log('verbose', `Application (http) started on port ${httpServer.address().address}:${httpServer.address().port}`);
     });
   } catch (err) {
     logger.error(`Unexpected exception: ${JSON.stringify(err)}`);
